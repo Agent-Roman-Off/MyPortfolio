6bf3bdc1cb34ae089d474987a3e909e7d3bdbc98
Romain Brehon <romain.brehon@yahoo.fr>
2022-02-02 22:14:21 +0100

    created the footer for desktop and responsive

--- src/components/Footer/style.jsx
+++ src/components/Footer/style.jsx
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
 const SFooter = styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--mColor);
+  width: 100%;
+  height: 70px;
+  color: var(--yColor);
   @media only screen and (min-width: 700px) {
   }
 `;
