a449a92a2c1f6149d024213ceefb7d23884aa5af
Romain Brehon <romain.brehon@yahoo.fr>
2022-02-03 11:40:13 +0100

    minor style and route fixes

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,25 +4,59 @@ import Project from './Item';
 import SHome from './style';
 
 function Home() {
-  const [projectList, setProjectList] = useState([]);
+  // FOR 2022 PROJECTS
+  const [project22List, setProject22List] = useState([]);
   useEffect(() => {
     axios
-      .get(`http://localhost:5050/projects`)
+      .get(`http://localhost:5050/projects/2022`)
       .then((res) => res.data)
       .then((data) => {
-        setProjectList(data);
+        setProject22List(data);
       })
       .catch((err) => {
-        setProjectList('Woops, there isnt anything here yet...');
+        setProject22List('Woops, there isnt anything here yet...');
+        console.log(err);
+      });
+  }, []);
+
+  // FOR 2021 PROJECTS
+  const [project21List, setProject21List] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5050/projects/2021`)
+      .then((res) => res.data)
+      .then((data) => {
+        setProject21List(data);
+      })
+      .catch((err) => {
+        setProject21List('Woops, there isnt anything here yet...');
         console.log(err);
       });
   }, []);
   return (
     <SHome>
+      {/* 2022 PROJECTS */}
       <div className="listCont">
         <h3 className="date">-- 2022 --</h3>
         <ul className="projectList">
-          {projectList.map((project) => (
+          {project22List.map((project) => (
+            <li className="projectItem">
+              <Project
+                key={project.id}
+                title={project.title}
+                picture={project.picture}
+                date={project.date}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* 2021 PROJECTS */}
+      <div className="listCont">
+        <h3 className="date">-- 2021 --</h3>
+        <ul className="projectList">
+          {project21List.map((project) => (
             <li className="projectItem">
               <Project
                 key={project.id}
